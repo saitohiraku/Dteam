92cf9b3e5e687f1b1b5f847fc17757df4aebbdf9
DESKTOP-87LMO7N\akary <Rkawaai0105@gmail.com>
2024-07-03 15:40:50 +0900

    観光地一覧のdb接続部分変更

--- sources/sightseeing.php
+++ sources/sightseeing.php
@@ -7,116 +7,90 @@
 
 //ライブラリをインクルード
 require_once("common/libs.php");
+// ログインチェックを行うファイルをインクルード
+require_once("auth_check.php");
 
 $err_array = array();
 $err_flag = 0;
 $page_obj = null;
 
-
 //--------------------------------------------------------------------------------------
-///	本体ノード
+/// 本体ノード
 //--------------------------------------------------------------------------------------
 class cmain_node extends cnode {
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	コンストラクタ
-	*/
-	//--------------------------------------------------------------------------------------
-	public function __construct() {
-		//親クラスのコンストラクタを呼ぶ
-		parent::__construct();
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief  本体実行（表示前処理）
-	@return なし
-	*/
-	//--------------------------------------------------------------------------------------
-	public function execute(){
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	構築時の処理(継承して使用)
-	@return	なし
-	*/
-	//--------------------------------------------------------------------------------------
-	public function create(){
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief  表示(継承して使用)
-	@return なし
-	*/
-	//--------------------------------------------------------------------------------------
-	public function display(){
-//PHPブロック終了
-?>
-<!-- コンテンツ　-->
-<h1>福島県の観光地一覧</h1>
+    private $destinations;
 
-<div class="itemArea">
-        <a href="./sightseeingDetail.php">
-            <div class="itemImg">
-                <img src="http://150.95.36.201/~k2024d/image/Ysiki.jpg" alt="image">
-            </div>
-            <div class="itemText">
-                <h2>観光地名</h2>
-            </div>
-        </a>
-        <a href="./sightseeingDetail.php">
-            <div class="itemImg">
-                <img src="http://150.95.36.201/~k2024d/image/Ysiki.jpg" alt="image">
-            </div>
-            <div class="itemText">
-                <h2>観光地名</h2>
-            </div>
-        </a>
-        <a href="./sightseeingDetail.php">
-            <div class="itemImg">
-                <img src="http://150.95.36.201/~k2024d/image/Ysiki.jpg" alt="image">
-            </div>
-            <div class="itemText">
-                <h2>観光地名</h2>
-            </div>
-        </a>
-        <a href="./sightseeingDetail.php">
-            <div class="itemImg">
-                <img src="http://150.95.36.201/~k2024d/image/Ysiki.jpg" alt="image">
-            </div>
-            <div class="itemText">
-                <h2>観光地名</h2>
-            </div>
-        </a>
-        <a href="./sightseeingDetail.php">
-            <div class="itemImg">
-                <img src="http://150.95.36.201/~k2024d/image/Ysiki.jpg" alt="image">
-            </div>
-            <div class="itemText">
-                <h2>観光地名</h2>
-            </div>
-        </a>
-        <a href="./sightseeingDetail.php">
-            <div class="itemImg">
-                <img src="http://150.95.36.201/~k2024d/image/Ysiki.jpg" alt="image">
-            </div>
-            <div class="itemText">
-                <h2>観光地名</h2>
-            </div>
-        </a>
+    //--------------------------------------------------------------------------------------
+    /*!
+    @brief コンストラクタ
+    */
+    //--------------------------------------------------------------------------------------
+    public function __construct() {
+        parent::__construct();
+    }
+
+    //--------------------------------------------------------------------------------------
+    /*!
+    @brief 本体実行（表示前処理）
+    @return なし
+    */
+    //--------------------------------------------------------------------------------------
+    public function execute() {
+        $touristDest = new ctd();
+        $this->destinations = $touristDest->get_all();
+    }
+
+    //--------------------------------------------------------------------------------------
+    /*!
+    @brief 構築時の処理(継承して使用)
+    @return なし
+    */
+    //--------------------------------------------------------------------------------------
+    public function create() {
+    }
+
+    //--------------------------------------------------------------------------------------
+    /*!
+    @brief 表示(継承して使用)
+    @return なし
+    */
+    //--------------------------------------------------------------------------------------
+    public function display() {
+        //PHPブロック終了
+?>
+<!-- コンテンツ -->
+<div class="contents">
+    <h1>福島県の観光地一覧</h1>
+    <div class="itemArea">
+        <?php if (!empty($this->destinations)): ?>
+            <?php foreach ($this->destinations as $row): ?>
+                <a href="./sightseeingDetail.php?TD_ID=<?php echo intval($row['TD_ID']); ?>">
+                    <div class="itemImg">
+                        <img src="<?php echo htmlspecialchars($row['TD_Photo'], ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($row['TD_Name'], ENT_QUOTES, 'UTF-8'); ?>">
+                    </div>
+                    <div class="itemText">
+                        <h2><?php echo htmlspecialchars($row['TD_Name'], ENT_QUOTES, 'UTF-8'); ?></h2>
+                    </div>
+                </a>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p>観光地情報がありません。</p>
+        <?php endif; ?>
     </div>
-<!-- /コンテンツ　-->
-<?php 
-//PHPブロック再開
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	デストラクタ
-	*/
-	//--------------------------------------------------------------------------------------
-	public function __destruct(){
-		//親クラスのデストラクタを呼ぶ
-		parent::__destruct();
-	}
+</div>
+<!-- /コンテンツ -->
+<?php
+        //PHPブロック再開
+    }
+
+    //--------------------------------------------------------------------------------------
+    /*!
+    @brief デストラクタ
+    */
+    //--------------------------------------------------------------------------------------
+    public function __destruct() {
+        parent::__destruct();
+    }
 }
 
 //ページを作成
